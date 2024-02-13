78e242ec8b28979ce0cc732b1b5d01ddc12eb572
Sandesh Gurung <sandeshgurung@Sandeshs-MacBook-Air.local>
2024-02-13 23:28:29 +0000

    employee.js codes added

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+// lib/Employee.js
+
+// Define the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    // Method to get the employee's name
+    getName() {
+        return this.name;
+    }
+
+    // Method to get the employee's ID
+    getId() {
+        return this.id;
+    }
+
+    // Method to get the employee's email
+    getEmail() {
+        return this.email;
+    }
+
+    // Method to get the employee's role (this method will be overridden by subclasses)
+    getRole() {
+        return 'Employee';
+    }
+}
+
+// Export the Employee class
+module.exports = Employee;
